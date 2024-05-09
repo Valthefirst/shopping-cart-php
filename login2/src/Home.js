d9c839293e3fa130189b3c27b99f6456f942f2bb
Zeena Nzigamasabo <142906579+zeenanzigamasabo@users.noreply.github.com>
2024-05-09 03:09:51 -0400

    Add files via upload

--- login2/src/Home.js
+++ login2/src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ItemCard from "./ItemCard";
 import data from "./data";
 
@@ -27,4 +27,43 @@ const Home=() =>{
   )
 }
 
-export default Home
+export default Home*/
+
+import React, { useState } from 'react';
+import ItemCard from "./ItemCard";
+import data from "./data";
+
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1 className="text-center mt-3">Zeena, Carlos and Val's junkyard</h1>
+      {/* Search bar */}
+      <div className="container mt-3">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="form-control mb-3"
+        />
+      </div>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          {data.productData
+            .filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()))
+            .map((item, index) => (
+              <ItemCard img={item.img} title={item.title} desc={item.desc} price={item.price} item={item} key={index}/>
+            ))}
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home;
